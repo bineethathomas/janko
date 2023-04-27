97090211e96d63ccd51afb26e4342dab631654cd
bineethathomas <bineethathomas@gmail.com>
2023-04-27 16:57:32 +0530

    change in yaml file

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,8 +1,8 @@
 import LoginPage from '../pageobjects/login.page.js';
 import SecurePage from '../pageobjects/secure.page.js';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+describe('My Login application sample', () => {
+    it('user should be able to login with valid credentials', async () => {
         await LoginPage.open();
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
